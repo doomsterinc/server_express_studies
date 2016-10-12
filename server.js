feb1c5d39c90ac6893b09875f2d0d9adbb9bdcbc
doomsterinc <doomsterinc1@gmail.com>
2016-10-12 19:24:40 -0300

    add server.js and first server 3

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var app = express();
 
 app.get('/', function(req, res){
   res.send('hello from the express!');
-  console.log("server online in localhost:3000");
 });
-
+app.get('/about', function(req, res){
+  res.send('About us!');
+});
+console.log("server online in localhost:3000");
 app.listen(3000);
