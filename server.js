20fa4676429b76b967ed0f401418dcbe9e87e9a1
doomsterinc <doomsterinc1@gmail.com>
2016-10-15 19:53:12 -0300

    refatoring code

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var app = express();
 const PORT = 3000;
-
-var middleware = {
-  requireAutentication: function (req,res, next) {
-      console.log("private route hit!");
-      next();
-  },
-  logger: function (req, res, next) {
-    var date = new Date().toString();
-    console.log("Request in " + date + ": " + req.method + " " + req.originalUrl);
-    next();
-  }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
